32074bcdf2409c66d13c8137f6d6d873c0e4c8c1
Scott McAllister <scott@ngrok.com>
2024-05-01 21:38:31 -0700

    trying cy config.env var

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -23,3 +23,4 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+config.env.FRONTEND_URL = process.env.FRONTEND_URL;
